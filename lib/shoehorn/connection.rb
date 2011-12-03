102cfd9c16ba960af37ac340bef38d9c2e9efd1a
Mike Gunderloy <MikeG1@larkfarm.com>
2011-12-03 11:53:46 -0600

    Add logger

--- lib/shoehorn/connection.rb
+++ lib/shoehorn/connection.rb
@@ -1,8 +1,36 @@
+require 'logger'
+
 module Shoehorn
   class Connection
-    
+
     API_VERSION = 1
     API_ENDPOINT = "https://api.shoeboxed.com/v#{API_VERSION}/ws/api.htm"
-    
+
+    def initialize
+      setup_logger
+    end
+
+    def logger
+      @@logger
+    end
+
+    def logger=(new_logger)
+      @@logger = new_logger
+    end
+
+    def log_level=(level)
+      @@logger.level = level
+    end
+
+    def log_level
+      @@logger.level
+    end
+
+private
+    def setup_logger
+      @@logger = Logger.new(STDOUT)
+      @@logger.level = Logger::WARN
+    end
+
   end
 end
--- test/connection_test.rb
+++ test/connection_test.rb
@@ -8,4 +8,30 @@ class TestConnection < Test::Unit::TestCase
       assert_equal "https://api.shoeboxed.com/v1/ws/api.htm", Shoehorn::Connection::API_ENDPOINT
     end
   end
+
+  context "logger" do
+    setup do
+      @connection = Shoehorn::Connection.new
+    end
+
+    should "set up a new logger by default" do
+      assert_not_nil @connection.logger
+    end
+
+    should "allow setting a logger" do
+      new_logger = Logger.new(STDOUT)
+      @connection.logger = new_logger
+      assert_equal new_logger, @connection.logger
+    end
+
+    should "default to warning" do
+      assert_equal Logger::WARN, @connection.log_level
+    end
+
+    should "allow setting the logging level" do
+      @connection.log_level = Logger::INFO
+      assert_equal Logger::INFO, @connection.log_level
+    end
+  end
+
 end
