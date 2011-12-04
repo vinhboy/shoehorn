a39ea5fd04114551b1660d35956f27d4ba82ce9e
Mike Gunderloy <MikeG1@larkfarm.com>
2011-12-04 11:19:50 -0600

    Flesh out Bill class

--- lib/shoehorn/bill.rb
+++ lib/shoehorn/bill.rb
@@ -1,5 +1,6 @@
 module Shoehorn
   class Bill
-    
+    attr_accessor :id, :envelope_code, :note, :create_date, :modify_date, :name, :document_currency, :account_currency,
+      :document_total, :converted_total, :formatted_document_total, :formatted_converted_total, :images
   end
 end
