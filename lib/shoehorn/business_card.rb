ffadd351912bc68fcb6f1a9ab344822c64ce1a02
Mike Gunderloy <MikeG1@larkfarm.com>
2011-12-05 08:10:06 -0600

    Add accessors to business_card

--- lib/shoehorn/business_card.rb
+++ lib/shoehorn/business_card.rb
@@ -1,5 +1,7 @@
 module Shoehorn
   class BusinessCard
-    
+     attr_accessor :id, :first_name, :last_name, :create_date, :address, :address2, :city, :state, :zip,
+      :country, :email, :website, :company, :position, :work_phone, :cell_phone, :fax, :front_img_url,
+      :back_img_url, :note
   end
 end
