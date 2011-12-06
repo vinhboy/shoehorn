24abe509df7268f319405445d84386df6b9eb6f5
Mike Gunderloy <MikeG1@larkfarm.com>
2011-12-06 11:50:38 -0600

    Properly define ParseError (h/t rshoeboxed)

--- lib/shoehorn/errors.rb
+++ lib/shoehorn/errors.rb
@@ -2,7 +2,25 @@ module Shoehorn
   class AuthenticationError < StandardError; end;
   class InternalError < StandardError; end;
   class ParameterError < StandardError; end 
-  class ParseError < StandardError; end
+
+  class ParseError < StandardError
+    attr_accessor :original_error, :xml
+  
+    def initialize(original_error, xml, msg = nil)
+      @original_error = original_error
+      @xml = xml
+      super(msg)
+    end
+    
+    def to_s
+      message = super
+      
+      "Original Error: #{original_error.to_s}\n" +
+      "XML: #{xml.to_s}\n" +
+      "Message: #{message}\n"
+    end
+  end
+
   class RestrictedIPError < StandardError; end;
   class UnknownAPICallError < StandardError; end;
   class XMLValidationError < StandardError; end;
